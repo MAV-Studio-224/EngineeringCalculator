18935ff46f66e27e198c697b09b700a35376ff64
Alexandr <aleksandarmartynenkov@yandex.ru>
2024-04-07 04:18:45 +0300

    Engineering Calculator 1.1 Изменения: -улучшен код (использование моделей представлений и связывания)

---
+++ app/src/main/java/com/hfad/engineeringcalculator/CalculatorFragment.kt
@@ -0,0 +1,43 @@
+package com.hfad.engineeringcalculator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hfad.engineeringcalculator.databinding.FragmentCalculatorBinding
+import androidx.lifecycle.ViewModelProvider
+
+class CalculatorFragment : Fragment() {
+    private var _binding: FragmentCalculatorBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: CalculatorViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentCalculatorBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        viewModel = ViewModelProvider(this)[CalculatorViewModel::class.java]
+
+        viewModel.outputStr.observe(viewLifecycleOwner) { newValue ->
+            binding.textView.text = newValue
+        }
+
+        viewModel.result.observe(viewLifecycleOwner) { newValue ->
+            binding.textView2.text = newValue
+        }
+
+        binding.viewModel = viewModel
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/hfad/engineeringcalculator/CalculatorViewModel.kt
@@ -0,0 +1,98 @@
+package com.hfad.engineeringcalculator
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel: ViewModel(){
+    private var _outputStr = MutableLiveData("")
+    val outputStr: LiveData<String>
+        get() = _outputStr
+    private var _result = MutableLiveData("")
+    val result: LiveData<String>
+        get() = _result
+
+    private fun eval(str: String?): Any {
+        return try {
+            if (str != null) {
+                var string: String = str
+                if ('√' in str) {
+                    for (i in 0..<(str.length-1)) {
+                        if (str[i] == '√') {
+                            val str1st = string.substringBefore(str[i])
+                            string = "SQRT(${string.substringAfter(str[i])}"
+                            for (j in 5..string.length) {
+                                if (j >= string.length ||
+                                    string[j] != '1' && string[j] != '2' && string[j] != '3' &&
+                                    string[j] != '4' && string[j] != '5' && string[j] != '6' &&
+                                    string[j] != '7' && string[j] != '8' && string[j] != '9'
+                                ) {
+                                    if (j >= string.length) {
+                                        string = "$str1st$string)"
+                                        break
+                                    }
+                                    val strLast = string.substringAfter(string[j])
+                                    string = "${string.substringBefore(string[j])})"
+                                    string = "$str1st$string$strLast"
+                                    break
+                                }
+                            }
+                        }
+                    }
+                }
+                val r = com.ezylang.evalex.Expression(string)
+                val l = r.evaluate().numberValue.toDouble()
+                if (l.toInt().toDouble() == l) {
+                    l.toInt()
+                } else {
+                    kotlin.math.round(l * 10000000) / 10000000
+                }
+            } else throw Exception("ERROR")
+        } catch (e: Exception) {
+            "ERROR"
+        }
+    }
+
+    fun addDot(){
+        if (outputStr.value != "" && (outputStr.value?.last() ?: ' ') != ' ' &&
+            (outputStr.value?.last() ?: '.') != '.' && (outputStr.value?.last() ?: '√') != '√') {
+            _outputStr.value = "${outputStr.value}."
+        } else if (outputStr.value != "" && (outputStr.value?.last() ?: ' ') != ' ' &&
+            ((outputStr.value?.last() ?: '.') == '.' && (outputStr.value?.last() ?: '√') == '√')) {
+            _outputStr.value = "${outputStr.value}0."
+        }
+    }
+
+    private fun add(str: String){
+        _outputStr.value += str
+    }
+
+    private fun addResult(){
+        _result.value = eval(outputStr.value).toString()
+    }
+
+    fun addAll(str: String){
+        add(str)
+        addResult()
+    }
+
+    fun addSymbol(str: String){
+        if (outputStr.value != "") {
+            _outputStr.value = "${outputStr.value}$str"
+        }
+    }
+
+    fun result() {
+        _outputStr.value = result.value
+    }
+
+    fun clear(){
+        _outputStr.value = ""
+        _result.value = ""
+    }
+
+    fun delete() {
+        _outputStr.value = _outputStr.value?.dropLast(1)
+        addResult()
+    }
+}
