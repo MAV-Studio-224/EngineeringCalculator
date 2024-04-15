12301a7f06dd9cb99536804e607f7c406b753959
Alexandr <aleksandarmartynenkov@yandex.ru>
2024-04-15 20:55:35 +0300

    Engineering Calculator 1.2 Изменения: -добавлена нижняя панель навигации -появилась заготовка для решений уравнений(фрагмент для этого экрана, а также фрагменты для линейных и квадратных уравнений)

--- app/src/main/java/com/hfad/engineeringcalculator/MainActivity.kt
+++ app/src/main/java/com/hfad/engineeringcalculator/MainActivity.kt
@@ -2,10 +2,21 @@ package com.hfad.engineeringcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.hfad.engineeringcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.host) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.menu.setupWithNavController(navController)
+
+        setContentView(view)
     }
 }
